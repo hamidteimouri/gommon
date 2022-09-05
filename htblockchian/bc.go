c3c00ecafa154e5e2aa6d0b79eee2f1d5a7eac7d
Hamid Teimouri <hamidteimouri@outlook.com>
2022-09-05 16:55:12 +0430

    regex package added and some bug fixed

---
+++ htblockchian/bc.go
@@ -0,0 +1,53 @@
+package htblockchian
+
+var (
+	NetworkBitcoinExplorer           = "https://blockchair.com/bitcoin/transaction/"
+	NetworkEthereumExplorer          = "https://etherscan.io/tx/"
+	NetworkBinanceSmartChainExplorer = "https://bscscan.com/tx/"
+	NetworkBinanceCoinExplorer       = "https://explorer.binance.org/tx/"
+	NetworkTronExplorer              = "https://blockchair.com/bitcoin/transaction/"
+
+	ChainBTC = "bitcoin"
+	ChainETH = "ethereum"
+	ChainBSC = "smartchain"
+	ChainBNB = "binance"
+	ChainTRX = "tron"
+
+	Chains = map[string]string{
+		ChainBTC: NetworkBitcoinExplorer,
+		ChainETH: NetworkEthereumExplorer,
+		ChainBSC: NetworkBinanceSmartChainExplorer,
+		ChainBNB: NetworkBinanceCoinExplorer,
+		ChainTRX: NetworkTronExplorer,
+	}
+)
+
+// GeneralConvertor converts a TXID to a clickable link for explorers
+func GeneralConvertor(chain, txID string) string {
+	return Chains[chain] + txID
+}
+
+// ConvertTxidToBSCExplorerLink converts a TXid to a Binance Smart Chain clickable link
+func ConvertTxidToBSCExplorerLink(txid string) (link string) {
+	return NetworkBinanceSmartChainExplorer + txid
+}
+
+// ConvertTxidToBNBExplorerLink converts a TXid to a Binance Coin clickable link
+func ConvertTxidToBNBExplorerLink(txid string) (link string) {
+	return NetworkBinanceCoinExplorer + txid
+}
+
+// ConvertTxidToBTCExplorerLink converts a TXid to a Bitcoin clickable link
+func ConvertTxidToBTCExplorerLink(txid string) (link string) {
+	return NetworkBitcoinExplorer + txid
+}
+
+// ConvertTxidToETHExplorerLink converts a TXid to a Ethereum clickable link
+func ConvertTxidToETHExplorerLink(txid string) (link string) {
+	return NetworkEthereumExplorer + txid
+}
+
+// ConvertTxidToTRXExplorerLink converts a TXid to a TRON clickable link
+func ConvertTxidToTRXExplorerLink(txid string) (link string) {
+	return NetworkTronExplorer + txid
+}
--- htcolog/colog.go
+++ htcolog/colog.go
@@ -1,4 +1,4 @@
-package colog
+package htcolog
 
 import (
 	"fmt"
@@ -47,98 +47,122 @@ func init() {
 
 }
 
+//DoGreen prints a string with green color
 func DoGreen(str string) {
 	fmt.Println(Green + str + Reset)
 }
 
+//DoYellow prints a string with yellow color
 func DoYellow(str string) {
 	fmt.Println(Yellow + str + Reset)
 }
 
+//DoBlue prints a string with blue color
 func DoBlue(str string) {
 	fmt.Println(Blue + str + Reset)
 }
 
+//DoRed prints a string with red color
 func DoRed(str string) {
 	fmt.Println(Red + str + Reset)
 }
 
+//DoPurple prints a string with purple color
 func DoPurple(str string) {
 	fmt.Println(Purple + str + Reset)
 }
 
+//DoCyan prints a string with cyan color
 func DoCyan(str string) {
 	fmt.Println(Cyan + str + Reset)
 }
 
+//DoWhite prints a string with white color
 func DoWhite(str string) {
 	fmt.Println(White + str + Reset)
 }
 
+//DoGray prints a string with red color
 func DoGray(str string) {
 	fmt.Println(Gray + str + Reset)
 }
 
+//DoBgYellow prints a string with yellow background
 func DoBgYellow(str string) {
 	fmt.Println(BackGroundYellow + str + Reset)
 }
 
+//DoBgRed prints a string with red background
 func DoBgRed(str string) {
 	fmt.Println(BackGroundRed + str + Reset)
 }
 
+//DoBgGreen prints a string with green background
 func DoBgGreen(str string) {
 	fmt.Println(BackGroundGreen + str + Reset)
 }
 
+//DoBgBlue prints a string with blue background
 func DoBgBlue(str string) {
 	fmt.Println(BackGroundBlue + str + Reset)
 }
 
+//DoBgMagenta prints a string with magenta background
 func DoBgMagenta(str string) {
 	fmt.Println(BackGroundMagenta + str + Reset)
 }
 
+//DoBgCyan prints a string with cyan background
 func DoBgCyan(str string) {
 	fmt.Println(BackGroundCyan + str + Reset)
 }
 
+//DoBgWhite prints a string with white background
 func DoBgWhite(str string) {
 	fmt.Println(BackGroundWhite + str + Reset)
 }
 
+//DoBgBrightBlack prints a string with bright black background
 func DoBgBrightBlack(str string) {
 	fmt.Println(BackGroundBrightBlack + str + Reset)
 }
 
+//DoBgBrightGreen prints a string with bright green background
 func DoBgBrightGreen(str string) {
 	fmt.Println(BackGroundBrightGreen + str + Reset)
 }
 
+//DoBgBrightRed prints a string with bright red background
 func DoBgBrightRed(str string) {
 	fmt.Println(BackGroundBrightRed + str + Reset)
 }
 
+//DoBgBrightYellow prints a string with bright yellow background
 func DoBgBrightYellow(str string) {
 	fmt.Println(BackGroundBrightYellow + str + Reset)
 }
 
+//DoBgBrightMagenta prints a string with bright magenta background
 func DoBgBrightMagenta(str string) {
 	fmt.Println(BackGroundBrightMagenta + str + Reset)
 }
 
+//DoBgBrightBlue prints a string with bright blue background
 func DoBgBrightBlue(str string) {
 	fmt.Println(BackGroundBrightBlue + str + Reset)
 }
 
+//DoBgBrightCyan prints a string with bright cyan background
 func DoBgBrightCyan(str string) {
 	fmt.Println(BackGroundBrightCyan + str + Reset)
 }
 
+//DoBgBrightWhite prints a string with bright white background
 func DoBgBrightWhite(str string) {
 	fmt.Println(BackGroundBrightWhite + str + Reset)
 }
 
+//MakeRed makes a red string
 func MakeRed(str string) string {
 	if str == "" {
 		return ""
@@ -146,6 +170,7 @@ func MakeRed(str string) string {
 	return fmt.Sprintf(Red + str + Reset)
 }
 
+//MakeGreen makes a green string
 func MakeGreen(str string) string {
 	if str == "" {
 		return ""
@@ -153,6 +178,7 @@ func MakeGreen(str string) string {
 	return fmt.Sprintf(Green + str + Reset)
 }
 
+//MakeBlue makes a blue string
 func MakeBlue(str string) string {
 	if str == "" {
 		return ""
