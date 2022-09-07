4693792fab271d10f678c51c70883f2565b6c8da
Hamid Teimouri <hamidteimouri@outlook.com>
2022-09-07 15:20:40 +0430

    random string generator added

---
+++ htrandom/random.go
@@ -0,0 +1,17 @@
+package htrandom
+
+import (
+	"crypto/rand"
+	"unsafe"
+)
+
+var alphabet = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func String(size int) string {
+	b := make([]byte, size)
+	rand.Read(b)
+	for i := 0; i < size; i++ {
+		b[i] = alphabet[b[i]%byte(len(alphabet))]
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
