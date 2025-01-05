1749ee6c1f16a64d305e20a44c4d9b45ade67de5
Hamid Teimouri <iamhamidteimouri@gmail.com>
2025-01-05 09:45:10 +0330

    random OTP added

--- random.go
+++ random.go
@@ -2,6 +2,7 @@ package gommon
 
 import (
 	"crypto/rand"
+	mathRand "math/rand"
 	"unsafe"
 )
 
@@ -14,4 +15,17 @@ func RandomString(size int) string {
 		b[i] = alphabet[b[i]%byte(len(alphabet))]
 	}
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+func RandomOtpCode(length int) string {
+	// Define the character set from which the code will be generated
+	charset := "0123456789" // You can include other characters if needed
+
+	// Generate the verification code
+	code := make([]byte, length)
+	for i := range code {
+		code[i] = charset[mathRand.Intn(len(charset))]
+	}
+
+	return string(code)
 }
