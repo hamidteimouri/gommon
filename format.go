85f85383751a70015b546139491f108026c6ad49
Hamid Teimouri <iamhamidteimouri@gmail.com>
2025-01-10 21:21:40 +0330

    changed

--- format.go
+++ format.go
@@ -1,6 +1,7 @@
 package gommon
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
 	"strings"
@@ -174,4 +175,29 @@ func CutPrecision(input string, precision int) string {
 	}
 	// If no fractional part, return the input as-is
 	return input
+}
+
+// FormatNumber adds commas as thousands separators to a number string.
+func FormatNumber(input string) string {
+	// Split the input into the integer and fractional parts
+	parts := strings.Split(input, ".")
+	integerPart := parts[0]
+
+	// Handle the integer part
+	var buffer bytes.Buffer
+	length := len(integerPart)
+	for i, char := range integerPart {
+		buffer.WriteRune(char)
+		if (length-i-1)%3 == 0 && i != length-1 {
+			buffer.WriteRune(',')
+		}
+	}
+
+	// Append the fractional part if it exists
+	if len(parts) > 1 {
+		buffer.WriteString(".")
+		buffer.WriteString(parts[1])
+	}
+
+	return buffer.String()
 }
