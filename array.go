434c5c3f7e7bcf87f774f8b0906cfd12e39cb7cd
Hamid Teimouri <iamhamidteimouri@gmail.com>
2025-05-14 20:07:20 +0330

    changed

--- array.go
+++ array.go
@@ -88,3 +88,15 @@ func IsSlice(v interface{}) bool {
 func IsArray(v interface{}) bool {
 	return IsSlice(v)
 }
+
+func RemoveDuplicates[T comparable](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	var list []T
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
